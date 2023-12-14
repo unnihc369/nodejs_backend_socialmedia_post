f156c36533f4be6e27eb87695807f6597cecbde8
unnihc <unnihc369@gmail.com>
2023-12-14 13:03:56 +0530

    resolved issues

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/AuthController.js";
+import { loginUser, registerUser } from "../Controllers/AuthController";
 
 const router = express.Router();
 
